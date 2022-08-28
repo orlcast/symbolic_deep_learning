3f449c79385c2f1bd490c104215ffd18f1fa59ea
DomenicoCaudo <72082596+DomenicoCaudo@users.noreply.github.com>
2022-08-28 17:00:34 +0200

    Update models.py

--- models.py
+++ models.py
@@ -246,9 +246,9 @@ class Mbuti_GN(GN_mbuti):
             return torch.sum((g.y - self.just_derivative(g))**2)
         if loss_type == 'abs':
             return torch.sum(torch.abs(g.y - self.just_derivative(g)))
-	if loss_type == 'pit': 
-	    if torch.abs(g.y - self.just_derivative(g))/g.y < perc : 
-		return 0
-	    else: 
-		return  torch.abs(g.y - self.just_derivative(g))
+		if loss_type == 'pit': 
+			if torch.abs(g.y - self.just_derivative(g))/g.y < perc : 
+				return 0
+			else: 
+				return  torch.abs(g.y - self.just_derivative(g))
 
